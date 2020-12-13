02f92b60d7c39e4a9fc880a8692d5fa1db037bbd
Hunter Miller <hunter@thisdot.co>
2020-12-13 17:55:23 -0600

    improving image support for popup

--- filters/cloudinary-sizes.js
+++ filters/cloudinary-sizes.js
@@ -1,5 +1,6 @@
 module.exports = function(id, base) {
     const sizes = [400, 800, 1200, 1600, 2000, 2400];
+    let end = id[0] === '/' ? id.slice(1) : id;
     
-    return sizes.map(size => `${base}/w_${size}/${id} ${size}w`).join(', ');
+    return sizes.map(size => `${base}/w_${size}/${end} ${size}w`).join(', ');
 }
