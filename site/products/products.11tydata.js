1e51c123217043d3b4e29f0c17b3fc1791c131a6
Hunter Miller <hunter@thisdot.co>
2021-07-03 14:28:31 -0500

    adding default image

--- site/products/products.11tydata.js
+++ site/products/products.11tydata.js
@@ -1,6 +1,7 @@
 const utils = require('../../utilities');
 
 module.exports = {
+    default_image: '/v1625337970/kartchners/logo_q8zfwf.jpg',
     eleventyComputed: {
         category: data => {
             if (data.category)
