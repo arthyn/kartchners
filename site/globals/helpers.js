523371363fca5fa6ac61e67e207f0650cf3cd14d
Hunter Miller <hunter@thisdot.co>
2021-10-02 19:51:51 -0500

    fixing urls

--- site/globals/helpers.js
+++ site/globals/helpers.js
@@ -2,5 +2,5 @@ module.exports = {
 
     // environment helper
     'environment': process.env.ELEVENTY_ENV,
-    'domain': process.env.DEPLOY_PRIME_URL
+    'domain': process.env.CONTEXT === 'production' ? process.env.URL : process.env.DEPLOY_PRIME_URL
 };
